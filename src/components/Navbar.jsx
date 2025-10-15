3090d1c068b44563f447d4324d9f0d517f55c218
pedrofonseca1227 <henrriquemaluko@gmail.com>
2025-10-14 21:10:16 -0300

    Mostrar usuário logado navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../hooks/useAuth';
 import './Navbar.css';
 
 const Navbar = () => {
+  const { user, logout } = useAuth();
+
   return (
     <nav className="navbar">
       <h1>🌿 Life Garden</h1>
       <ul>
         <li><Link to="/">Início</Link></li>
         <li><Link to="/sobre">Sobre</Link></li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/cadastro">Cadastro</Link></li>
+        {!user ? (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/cadastro">Cadastro</Link></li>
+          </>
+        ) : (
+          <>
+            <li>Olá, {user.email}</li>
+            <li><button onClick={logout}>Sair</button></li>
+          </>
+        )}
       </ul>
     </nav>
   );
