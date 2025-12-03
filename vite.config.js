5f88349e4b7900d1a391e465fd3dc880038f93ba
pedrofonseca1227 <henrriquemaluko@gmail.com>
2025-12-03 18:15:55 -0300

    add sonarcloud integracao

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,18 @@ export default defineConfig({
     globals: true,
     environment: "jsdom",
 
-    // GARANTE que o Vitest NÃO rode testes E2E
     include: ["tests/unit/**/*.test.js", "tests/integration/**/*.test.js"],
-
-    // Impede que Vitest tente carregar arquivos E2E ou configs do Playwright
     exclude: [
       "node_modules/**",
       "dist/**",
       "tests/e2e/**",
       "playwright.config.js"
     ],
+
+    coverage: {
+      provider: 'v8',          
+      reporter: ['lcov', 'text'],
+      reportsDirectory: './coverage'
+    }
   }
 })
